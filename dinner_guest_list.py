21367bc4ffad63404a902807923f1534d33e5e8d
Tahsin2278 <nazmussaqibtahsin2007@gmail.com>
2026-03-05 13:30:36 -0800

    commit 1
    I made a main function in which i wrote down the comments about the things i need to work on

---
+++ dinner_guest_list.py
@@ -0,0 +1,14 @@
+def main():
+    pass
+    # ask the user for his name
+    # welcom the user to the dinner.
+    # ask the user to choose from the options
+    # based on the choice the user made run features
+    # features
+    # add guests
+    # .Modify guest.
+    # Remove guest.
+    # Sort guests.
+    # Show number of guests.
+    # Show invitations.
+    # exit
