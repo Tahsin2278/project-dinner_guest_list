e0cb6631cbc56b4d7d9a3abd8225840e79b5ba63
Tahsin2278 <nazmussaqibtahsin2007@gmail.com>
2026-03-06 14:37:38 -0800

    commit 2
    I created a function to ask the user for their name.

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -1,14 +1,38 @@
+guest = []
+
+
+def ask_name():
+    try:
+        name = input("Enter your name: ").strip().title()
+    except ValueError:
+        print("Invalid name. Please enter a valid name.")
+    return name
+
+
 def main():
-    pass
+
     # ask the user for his name
-    # welcom the user to the dinner.
+
+    # welcome the user to the dinner.
+
     # ask the user to choose from the options
+
     # based on the choice the user made run features
+
     # features
+
     # add guests
+
     # .Modify guest.
+
     # Remove guest.
+
     # Sort guests.
+
     # Show number of guests.
+
     # Show invitations.
+
     # exit
+    if __name__ == "__main__":
+        main()
